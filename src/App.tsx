725a2048ac0e280aa84125626e8c9981d25f0cd1
vdanilchuk <d.vladislav@tut.by>
2023-05-07 17:59:23 +0200

    Microtasks useState [Finished]

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import React from 'react';
-import './App.css';
-import {MouseEvent} from "react";
-import {Button} from "./button_microtask/Button";
+import React, {useState} from "react";
+import "./App.css";
 
 function App() {
 
+    let [a, setA] = useState(1)
 
-    const FunctForButton1 = (anytext: string, words: string) => {
-        console.log(anytext, words)
+    const onClickHandler = () => {
+        setA(++a);
+        console.log(a);
     }
-    const FunctForButton2 = (anytext: string) => {
-        console.log(anytext)
-    }
-
-    const FunctForButton3 = () => {
-        console.log('I am button')
+    const onClickHandler2 = () => {
+        setA(0);
+        console.log(a);
     }
 
     return (
         <div className="App">
-            <Button title='Button 1' callback={() => FunctForButton1('1', 'YES')}/>
-            <Button title='Button 2' callback={() => FunctForButton2('2')}/>
-            <Button title='Button 3' callback={FunctForButton3}/>
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>Change Number</button>
+            <button onClick={onClickHandler2}>Reset</button>
         </div>
     );
 }
--- src/button_microtask/Button.tsx
+++ src/button_microtask/Button.tsx
@@ -1,4 +1,4 @@
-import React, {MouseEvent} from "react";
+import React from "react";
 
 type ButtonType = {
     title: string
